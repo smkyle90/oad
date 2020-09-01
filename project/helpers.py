8ab56caca29c135b10d9e138e5587f20253d3c2b
Scott Kyle <scott.m.kyle@gmail.com>
2020-09-01 18:27:49 -0400

    ran pre-commit

--- project/helpers.py
+++ project/helpers.py
@@ -1,9 +1,11 @@
 """App helper functions
 """
 
+
 def get_picks():
-    avail_picks = ['TW', 'PM', 'DJ', 'AS']
+    avail_picks = ["TW", "PM", "DJ", "AS"]
     return avail_picks
+
 
 def get_event():
     return "Test Open"
