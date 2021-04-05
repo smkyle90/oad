f7a44c223de94d318671c85d43a9c2783ff8ce5a
Scott Kyle <scott.m.kyle@gmail.com>
2021-04-05 13:04:13 -0400

    post tourney table update to replace NaN in earnings df

--- oad/views.py
+++ oad/views.py
@@ -140,6 +140,11 @@ def weekly_pick_table(users, picks, event_info, user_data):
 
     # Display table based on if earnings are published
     if df["earnings"].sum():
+        print(df.pos)
+        df["pos"] = df["pos"].fillna(-1)
+        df["pos"] = df["pos"].astype(int)
+        df["pos"] = df["pos"].replace(-1, "CUT")
+
         df["earnings"] = [format_earnings(earnings) for earnings in df["earnings"]]
 
         df = df[["team", "pick", "tot", "pos", "earnings"]]
