2f16f3f0abfad1e31a946cc619ef3b36ef2338af
Scott Kyle <scott.m.kyle@gmail.com>
2023-07-11 11:37:44 -0400

    update helpers

--- oad/util/helpers.py
+++ oad/util/helpers.py
@@ -19,7 +19,7 @@ https://pthree.org/2012/01/07/encrypted-mutt-imap-smtp-passwords/
 https://gist.github.com/bnagy/8914f712f689cc01c267
 """
 
-EVENT_TYPE = "major"
+EVENT_TYPE = "regular"
 
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, "points.csv")
@@ -68,8 +68,8 @@ def send_email(receiver_email, subject, html):
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
     sender_email = "1993oad@gmail.com"
-    password = os.getenv("OADPW")
-
+    password = "iuoeigckonvtfhns"
+    
     message = "{}".format(html)
     msg = MIMEText(message, "html")
     msg["Subject"] = subject
